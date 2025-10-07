1639b6c30c7742d14552845c296563820abb7a0a
udaya <udayasriramagundam@gmail.com>
2025-10-07 11:40:56 +0530

    initial commit

---
+++ src/test/java/com/example/my_maven_project/AppTest.java
@@ -0,0 +1,19 @@
+package com.example.my_maven_project;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
